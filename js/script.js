ffadd8977c90550f7c44e67207de0c9dc10fa1bb
Roman Mironov <thiefon@gmail.com>
2019-02-22 20:33:15 +0500

    добавил анимацию сворачивания формы

--- js/script.js
+++ js/script.js
@@ -3,9 +3,17 @@ var popup = document.querySelector(".popup");
 var dateIn = popup.querySelector("[name=date-in]");
 var form = popup.querySelector(".search-form");
 
+
+form.classList.add("search-form-close");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.toggle("popup-show");
-  form.classList.toggle("search-form-show");
+  form.classList.toggle("search-form-open");
+  form.classList.toggle("search-form-close");
+  if(popup.classList.contains('popup-show')) 
+    {
+    setTimeout(function() {popup.classList.toggle("popup-show")},500);
+  } else {
+    popup.classList.toggle("popup-show");
+  }
   dateIn.focus();
 });

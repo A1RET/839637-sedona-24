6ec22f23b385488cc6e23280cd5b5fed27a25b7d
Roman Mironov <thiefon@gmail.com>
2019-02-22 20:37:41 +0500

    убрал лишние переносы строк

--- js/script.js
+++ js/script.js
@@ -3,14 +3,12 @@ var popup = document.querySelector(".popup");
 var dateIn = popup.querySelector("[name=date-in]");
 var form = popup.querySelector(".search-form");
 
-
 form.classList.add("search-form-close");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   form.classList.toggle("search-form-open");
   form.classList.toggle("search-form-close");
-  if(popup.classList.contains('popup-show')) 
-    {
+  if(popup.classList.contains('popup-show')) {
     setTimeout(function() {popup.classList.toggle("popup-show")},500);
   } else {
     popup.classList.toggle("popup-show");

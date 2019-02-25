eb7692637daa5c0f778a4db96bb7447d42bae808
Roman Mironov <thiefon@gmail.com>
2019-02-25 14:47:12 +0500

    Сделал форму видимой по-умолчанию

--- js/script.js
+++ js/script.js
@@ -3,15 +3,16 @@ var popup = document.querySelector(".popup");
 var dateIn = popup.querySelector("[name=date-in]");
 var form = popup.querySelector(".search-form");
 
+popup.classList.add("popup-hide");
 form.classList.add("search-form-close");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   form.classList.toggle("search-form-open");
   form.classList.toggle("search-form-close");
-  if(popup.classList.contains('popup-show')) {
-    setTimeout(function() {popup.classList.toggle("popup-show")},500);
+  if(popup.classList.contains('popup-hide')) {
+    popup.classList.toggle("popup-hide");
   } else {
-    popup.classList.toggle("popup-show");
+    setTimeout(function() {popup.classList.toggle("popup-hide")},500);
   }
   dateIn.focus();
 });
